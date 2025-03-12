ed045661bd8b7803da21d07c2e72481b2417c1ab
ZuoYan <750945715@qq.com>
2025-03-12 14:26:30 +0800

    适配Linux

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,9 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-//head = [-2, 5, -1, -10, 1, -1, 10, 2, 2, 2, 5, 5] //ʾ������										  
+//head = [-2, 5, -1, -10, 1, -1, 10, 2, 2, 2, 5, 5] //示例链表										  
 //Definition for singly-linked list.
 
 struct ListNode 
@@ -16,7 +16,7 @@ struct ListNode
 	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 		
-//�����۳˺���������������ǰ�ڵ㣬��ǰ�ڵ�����һ���ڵ���ˣ����ؽ��
+//链表累乘函数，输入链表当前节点，当前节点与下一个节点相乘，返回结果
 int multipl(ListNode* listnode)
 {
 	int result;
@@ -55,7 +55,7 @@ void Solution::PrintList(bool IfHead, bool IFCycle)
 
 		printf("%d ", temp->val);
 		temp = temp->next;
-		while (temp != head) {								//��ӡ 
+		while (temp != head) {								//打印 
 			printf("%d ", temp->val);
 			temp = temp->next;
 		}
@@ -64,7 +64,7 @@ void Solution::PrintList(bool IfHead, bool IFCycle)
 	else
 	{
 
-		while (temp != NULL) {								//��ӡ 
+		while (temp != NULL) {								//打印 
 			printf("%d ", temp->val);
 			temp = temp->next;
 		}
@@ -74,23 +74,23 @@ void Solution::PrintList(bool IfHead, bool IFCycle)
 
 ListNode* Solution::InitialList(bool IfHead,bool IFCycle)
 {
-	//���������������ȣ������ʼ������Ԫ��
-	//�������������ͷ�ڵ㡢�Ƿ�ѭ������ʱ�������������,�Ƿ�˫����Ӱ�죩
-	///�ȿ�����ͷ�ڵ㡢��ѭ�������
-	head = (ListNode*)malloc(sizeof(ListNode));			//��̬�ڴ���� 
-	ListNode *temp = head;								//����һ����ʱָ�룬����ָ��ͷ��� 
+	//主动控制链表长度，随机初始化具体元素
+	//各种情况，有无头节点、是否循环（暂时考虑这四种请款,是否双向无影响）
+	///先考虑无头节点、无循环的情况
+	head = (ListNode*)malloc(sizeof(ListNode));			//动态内存分配 
+	ListNode *temp = head;								//创建一个临时指针，并且指向头结点 
 
 	int key;
-	printf("���������ڵ�ֵ��00������\n");
+	printf("输入链表节点值，00结束：\n");
 	if (!IfHead)
 	{
-		scanf("%d", &temp->val);						//�Ӽ�������ͷ����ֵ 
+		scanf("%d", &temp->val);						//从键盘输入头结点的值 
 	}
 
-	scanf("%d", &key);									//�Ӽ�������ͷ����ֵ
-	while (key != 00)									//ֻҪ������00�ͻ�һֱ�����µĽ�� 
+	scanf("%d", &key);									//从键盘输入头结点的值
+	while (key != 00)									//只要不输入00就会一直创建新的结点 
 	{	
-		//�������������̣���������һ���ڵ�֮ǰ���ж� �Ƿ�����һ��ֵ
+		//控制下整体流程，在申请下一个节点之前先判断 是否有下一个值
 		ListNode *a = (ListNode*)malloc(sizeof(ListNode));
 		temp->next = a;
 		temp = a;
@@ -117,7 +117,7 @@ ListNode* Solution::InitialList(bool IfHead,bool IFCycle)
 
 void Solution::removeSublists(bool IfHead, bool IFCycle)
 {
-	//�����㷨����ͷ��β���������������жϺ����һ��Ԫ�ؽ���ɾ�����������¸�Ԫ�ؽ����жϣ��������ƣ�һ�鼴��
+	//基本算法：从头到尾遍历整个链表，判断后对下一个元素进行删减，再与下下个元素进行判断，依次类推，一遍即可
 	ListNode *temp, *pretemp;
 	ListNode *temp1, *temp2 , *temp3;
 	ListNode *standard1, *standard2;
@@ -152,7 +152,7 @@ void Solution::removeSublists(bool IfHead, bool IFCycle)
 
 		a_Cycle = 0;
 
-		//�ݲ�����ѭ��
+		//暂不考虑循环
 		while (temp != standard1 || cycle)
 		{
 			temp1 = temp->next;	
@@ -177,14 +177,14 @@ void Solution::removeSublists(bool IfHead, bool IFCycle)
 				{
 					if (temp1->next == standard2&&result == 10)
 					{
-						temp3 = standard2;							//��¼ɾ����ĵ�һ���ڵ�
+						temp3 = standard2;							//记录删除后的第一个节点
 					}
 					else
 					{
-						temp3 = temp1;								//��¼ɾ����ĵ�һ���ڵ�
+						temp3 = temp1;								//记录删除后的第一个节点
 					}
 
-					if (IfHead)										//��¼ͷ
+					if (IfHead)										//记录头
 					{
 						if (temp == head->next)
 						{
@@ -215,7 +215,7 @@ void Solution::removeSublists(bool IfHead, bool IFCycle)
 						}
 					}
 
-					//ɾ���� temp ����ǰ temp1 �����нڵ�
+					//删掉从 temp 到当前 temp1 的所有节点
 					while (temp != temp3)
 					{
 						temp2 = temp;
@@ -259,35 +259,37 @@ void Solution::removeSublists(bool IfHead, bool IFCycle)
 	}
 
 
-	//��ǰ�ڵ���֮������нڵ������۳ˣ��������˻�Ϊ10��ɾ���۳ɵ����нڵ�(�������ֵ����10�˳�ѭ��)�����������һ���ڵ���ж�
-	//��ͷ�ڵ�ʱ Head �Ĺ�������(��һ���ڵ�ɾ����Ҫ���⿼�ǣ������ڵ㲻��Ҫ)
-	//ĩβʱ��Ҫ�����жϣ�����ĩβʱָ��ָ�� NULL
-	//ͷ�ڵ���Ҫ��¼ͷ��ָ�룬����ڵ���Ҫ��¼�ϸ��ڵ��ָ��
-	//ѭ������ͷ�ڵ�����ֻ�ǰ��ж�������NULLת��ΪHead����
-	//ѭ������ͷ�ڵ���������ֱ�������ˣ����಻�ùܣ�����һ��NULL��ΪHead����
+	//当前节点与之后的所有节点依次累乘，如果满足乘积为10，删除累成的所有节点(如果绝对值大于10退出循环)；否则进入下一个节点的判断
+	//无头节点时 Head 的归属问题(第一个节点删减需要特殊考虑，其它节点不需要)
+	//末尾时需要进行判断，当是末尾时指针指向 NULL
+	//头节点需要记录头的指针，其余节点需要记录上个节点的指针
+	//循环中有头节点的情况只是把判断依据由NULL转换为Head即可
+	//循环中无头节点的情况，就直接相连了，其余不用管，将第一个NULL换为Head即可
 
 	PrintList(IfHead, IFCycle);
 }
 
-void main()
+int main()
 {
 	Solution test;
 	bool IfHead = false, IFCycle = false;
 	int key;
-	printf("�����Ƿ���ͷ�ڵ�(0-�ޣ�1-��)��\n");
+	printf("链表是否有头节点(0-无；1-有)：\n");
 	scanf("%d", &key);
 	if (key != 0)
 	{
 		IfHead = true;
 	}
-	printf("�Ƿ�Ϊѭ������(0-�ǣ�1-��)��\n");
+	printf("是否为循环链表(0-非；1-是)：\n");
 	scanf("%d", &key);
 	if (key != 0)
 	{
 		IFCycle = true;
 	}
 	
-	test.InitialList(IfHead, IFCycle);							//�Ƿ���ͷ�ڵ㣬�Ƿ�ѭ��
+	test.InitialList(IfHead, IFCycle);							//是否有头节点，是否循环
 	test.removeSublists(IfHead, IFCycle);
 	system("pause");
+
+	return 1;
 }
